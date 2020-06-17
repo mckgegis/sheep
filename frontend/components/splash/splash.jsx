8326bb5e4e2c8f2ce8a77ab00922a72d168a433e
mckgegis <mckgegis@yahoo.com>
2020-06-16 19:24:10 -0700

    stylecode

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Splash = () => {
-    window.scrollTo(0,0);
-    return (
-        <Link to='/sneakers/45'>
-            <div className="splash-image-container">
-                <h2 className='splash-text'>KILLSHOT X APP ACADEMY</h2>
-            </div>
-        </Link>
-    )
+  window.scrollTo(0, 0);
+  return (
+    <Link to="/sneakers/45">
+      <div className="splash-image-container">
+        <h2 className="splash-text">KILLSHOT X APP ACADEMY</h2>
+      </div>
+    </Link>
+  );
+};
 
-} 
-
-
-export default Splash
+export default Splash;
