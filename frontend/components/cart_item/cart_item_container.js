ec28bf4dab4ccb7aa6aa8e7abbfd5bbf7422c632
mckgegis <mckgegis@yahoo.com>
2020-06-16 19:28:06 -0700

    stylecode

--- frontend/components/cart_item/cart_item_container.js
+++ frontend/components/cart_item/cart_item_container.js
@@ -1,23 +1,29 @@
-import { connect } from 'react-redux'
-import { clearCartItems, addItem, fetchCartItems, removeItem } from '../../actions/cart_item_action'
-import CartItemIndex from './cart_item_index'
+import { connect } from "react-redux";
+import {
+  clearCartItems,
+  addItem,
+  fetchCartItems,
+  removeItem
+} from "../../actions/cart_item_action";
+import CartItemIndex from "./cart_item_index";
 
 const mapStateToProps = state => {
-    const cartItems = Object.values(state.entities.cartItems) 
-    return ({
-        cartItems,
-        totalValue: (cartItems.length) ? Object.values(cartItems).map(el => el.price).reduce( (acc,el) => acc+el, 0)  : 0
-    })
+  const cartItems = Object.values(state.entities.cartItems);
+  return {
+    cartItems,
+    totalValue: cartItems.length
+      ? Object.values(cartItems)
+          .map(el => el.price)
+          .reduce((acc, el) => acc + el, 0)
+      : 0
+  };
 };
 
 const mapDispatchToProps = dispatch => ({
-    clearCartItems: () => dispatch(clearCartItems()),
-    addItem: (item) => dispatch(addItem(item)),
-    fetchCartItems: () => dispatch(fetchCartItems()),
-    removeItem: (itemId) => dispatch(removeItem(itemId))
+  clearCartItems: () => dispatch(clearCartItems()),
+  addItem: item => dispatch(addItem(item)),
+  fetchCartItems: () => dispatch(fetchCartItems()),
+  removeItem: itemId => dispatch(removeItem(itemId))
 });
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-) (CartItemIndex)
+export default connect(mapStateToProps, mapDispatchToProps)(CartItemIndex);
