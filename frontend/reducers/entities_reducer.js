b239544890e7a0baef14dafb72537a4320c069a3
mckgegis <mckgegis@yahoo.com>
2020-06-16 19:34:09 -0700

    stylecode

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,16 +1,16 @@
-import { combineReducers } from 'redux';
-import usersReducer from './users_reducer'
-import sneakerReducer from './sneaker_reducer'
-import listingsReducer from './listings_reducer'
-import cartItemReducer from './cart_items_reducer'
-import apparelReducer from './apparel_reducer'
+import { combineReducers } from "redux";
+import usersReducer from "./users_reducer";
+import sneakerReducer from "./sneaker_reducer";
+import listingsReducer from "./listings_reducer";
+import cartItemReducer from "./cart_items_reducer";
+import apparelReducer from "./apparel_reducer";
 
 const entitiesReducer = combineReducers({
-    users: usersReducer,
-    sneakers: sneakerReducer,
-    apparels: apparelReducer,
-    listings: listingsReducer,
-    cartItems: cartItemReducer
-})
+  users: usersReducer,
+  sneakers: sneakerReducer,
+  apparels: apparelReducer,
+  listings: listingsReducer,
+  cartItems: cartItemReducer
+});
 
 export default entitiesReducer;
--- frontend/util/sneaker_api_util.js
+++ frontend/util/sneaker_api_util.js
@@ -1,14 +1,12 @@
-export const fetchSneakers = (maxId) => (
-    $.ajax({
-        method: 'GET',
-        url: '/api/sneakers',
-        data: { maxId }
-    })
-)
+export const fetchSneakers = maxId =>
+  $.ajax({
+    method: "GET",
+    url: "/api/sneakers",
+    data: { maxId }
+  });
 
-export const fetchSneaker = (sneakerId) => (
-    $.ajax({
-        method: 'GET',
-        url: `/api/sneakers/${sneakerId}`
-    })
-)
+export const fetchSneaker = sneakerId =>
+  $.ajax({
+    method: "GET",
+    url: `/api/sneakers/${sneakerId}`
+  });
