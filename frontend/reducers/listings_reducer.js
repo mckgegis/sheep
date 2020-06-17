70a31d38335c3a0e569ceb7fc058094ec02c805a
mckgegis <mckgegis@yahoo.com>
2020-06-16 19:32:06 -0700

    stylecode

--- frontend/reducers/listings_reducer.js
+++ frontend/reducers/listings_reducer.js
@@ -1,25 +1,24 @@
-import { RECEIVE_LISTINGS } from '../actions/listing_action'
-import { RECEIVE_SNEAKER} from '../actions/sneaker_action'
-import { RECEIVE_APPAREL } from '../actions/apparel_action'
-import { CLEAR_LISTINGS } from '../actions/listing_action'
+import { RECEIVE_LISTINGS } from "../actions/listing_action";
+import { RECEIVE_SNEAKER } from "../actions/sneaker_action";
+import { RECEIVE_APPAREL } from "../actions/apparel_action";
+import { CLEAR_LISTINGS } from "../actions/listing_action";
 
+const listingsReducer = (state = {}, action) => {
+  Object.freeze(state);
 
-const listingsReducer = (state={}, action) => {
-    Object.freeze(state)
-
-    let nextState = Object.assign({}, state)
-    switch(action.type) {
-        case RECEIVE_LISTINGS:
-            return action.listings;
-        case RECEIVE_SNEAKER:
-            return action.payload.listings;
-        case RECEIVE_APPAREL:
-            return action.payload.listings;
-        case CLEAR_LISTINGS:
-            return {}
-        default:
-            return state;
-    }
+  let nextState = Object.assign({}, state);
+  switch (action.type) {
+    case RECEIVE_LISTINGS:
+      return action.listings;
+    case RECEIVE_SNEAKER:
+      return action.payload.listings;
+    case RECEIVE_APPAREL:
+      return action.payload.listings;
+    case CLEAR_LISTINGS:
+      return {};
+    default:
+      return state;
+  }
 };
 
-export default listingsReducer
+export default listingsReducer;
