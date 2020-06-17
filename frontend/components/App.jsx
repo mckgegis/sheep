ed5345e603619ce982560191f68b53950a4cbfc6
mckgegis <mckgegis@yahoo.com>
2020-06-16 19:26:56 -0700

    stylecode

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,34 +1,34 @@
 import React from "react";
-import { Route } from 'react-router-dom'
-import  SignupFormContainer from '../components/login/sign_up_form_container'
-import LoginFormContainer from '../components/login/login_form_container'
-import NavBarContainer from '../components/header/navbar_container'
-import SneakerIndexContainer from '../components/sneakers/sneaker_index_container'
-import Splash from '../components/splash/splash'
-import { AuthRoute, ProtectedRoute } from '../util/route_utils'
-import SneakerShowContainer from "../components/sneakers/sneaker_show_container"
-import CartItemContainer from "../components/cart_item/cart_item_container"
-import SearchContainer from "../components/search/search_container"
-import Style from '../components/styles/styles'
-import Footer from '../components/footer/footer'
-import ApparelIndexContainer from '../components/apparels/apparel_index_container'
-import ApparelShowContainer from '../components/apparels/apparel_show_container'
+import { Route } from "react-router-dom";
+import SignupFormContainer from "../components/login/sign_up_form_container";
+import LoginFormContainer from "../components/login/login_form_container";
+import NavBarContainer from "../components/header/navbar_container";
+import SneakerIndexContainer from "../components/sneakers/sneaker_index_container";
+import Splash from "../components/splash/splash";
+import { AuthRoute, ProtectedRoute } from "../util/route_utils";
+import SneakerShowContainer from "../components/sneakers/sneaker_show_container";
+import CartItemContainer from "../components/cart_item/cart_item_container";
+import SearchContainer from "../components/search/search_container";
+import Style from "../components/styles/styles";
+import Footer from "../components/footer/footer";
+import ApparelIndexContainer from "../components/apparels/apparel_index_container";
+import ApparelShowContainer from "../components/apparels/apparel_show_container";
 
 const App = () => (
-    <div > 
-        <NavBarContainer />
-        <Route exact path="/" component={Splash} />
-        <AuthRoute path="/login" component={LoginFormContainer} />
-        <AuthRoute path="/signup" component={SignupFormContainer} />
-        <Route exact path="/sneakers" component={SneakerIndexContainer} />
-        <Route path="/sneakers/:sneakerId" component={SneakerShowContainer} />
-        <Route exact path="/apparels" component={ApparelIndexContainer} />
-        <Route path="/apparels/:apparelId" component={ApparelShowContainer} />
-        <ProtectedRoute path='/cart' component={CartItemContainer} />
-        <Route path='/search' component={SearchContainer} />
-        <Route path='/styles' component={Style} />
-        <Footer />
-    </div>
+  <div>
+    <NavBarContainer />
+    <Route exact path="/" component={Splash} />
+    <AuthRoute path="/login" component={LoginFormContainer} />
+    <AuthRoute path="/signup" component={SignupFormContainer} />
+    <Route exact path="/sneakers" component={SneakerIndexContainer} />
+    <Route path="/sneakers/:sneakerId" component={SneakerShowContainer} />
+    <Route exact path="/apparels" component={ApparelIndexContainer} />
+    <Route path="/apparels/:apparelId" component={ApparelShowContainer} />
+    <ProtectedRoute path="/cart" component={CartItemContainer} />
+    <Route path="/search" component={SearchContainer} />
+    <Route path="/styles" component={Style} />
+    <Footer />
+  </div>
 );
 
 export default App;
--- frontend/components/sneakers/sneaker_show.jsx
+++ frontend/components/sneakers/sneaker_show.jsx
@@ -1,133 +1,120 @@
-import React from 'react' 
-import { Link, Switch, Route } from 'react-router-dom'
-import ListingIndex from '../listings/listings_index'
-import ListingIndexItem from '../listings/listing_index_item'
-import Sneaker from './sneaker'
-
-
+import React from "react";
+import { Link, Switch, Route } from "react-router-dom";
+import ListingIndex from "../listings/listings_index";
+import ListingIndexItem from "../listings/listing_index_item";
+import Sneaker from "./sneaker";
 
 class SneakerShow extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    this.props.fetchSneaker(this.props.match.params.sneakerId);
+  }
+
+  displayPrice() {
+    let values = Object.values(this.props.listings);
+    let prices = values.map(listing => listing.price);
+    return Math.min(...prices);
+  }
+
+  render() {
+    if (!this.props.sneaker) {
+      return null;
     }
 
-    componentDidMount() {
-        window.scrollTo(0, 0);
-        this.props.fetchSneaker(this.props.match.params.sneakerId)
+    let { sneaker } = this.props;
 
-    };
+    return (
+      <div>
+        <div className="sneaker-show-container">
+          <div className="sneaker-show-image-container">
+            <img src={sneaker.imageUrl} className="sneaker-show-image" />
+            <h1 className="sneaker-details">
+              {sneaker.brand} / {sneaker.silhouette} / {sneaker.name}
+            </h1>
+          </div>
+          <div className="sneaker-show-right-container">
+            <Switch>
+              <Route
+                path="/sneakers/:sneakerId/listings/:listingId"
+                render={props => (
+                  <ListingIndexItem
+                    props={props}
+                    product={this.props.sneaker}
+                    listings={this.props.listings}
+                    currentUser={this.props.user}
+                    addItem={this.props.addItem}
+                  />
+                )}
+              />
+              <Route
+                path="/sneakers/:sneakerId/listings/"
+                render={() => (
+                  <ListingIndex
+                    listings={Object.values(this.props.listings).sort(function(
+                      a,
+                      b
+                    ) {
+                      return a.size - b.size;
+                    })}
+                    type="sneakers"
+                  />
+                )}
+              />
+              <Route
+                path="/sneakers/:sneakerId"
+                render={() => (
+                  <Sneaker
+                    props={this.props}
+                    displayPrice={this.displayPrice()}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
+        </div>
 
-    displayPrice () {
-        let values = Object.values(this.props.listings)
-        let prices = values.map(listing => listing.price)
-        return Math.min(...prices)
-    }
-
-    render() {
-
-        if(!this.props.sneaker) {
-            return null
-        } 
-
-        
-       
-
-        let { sneaker } = this.props
-
-        return(
-            <div>
-
-                <div className='sneaker-show-container'>
-                    <div className='sneaker-show-image-container'>
-                        <img src={sneaker.imageUrl} className='sneaker-show-image' />
-                        <h1 className='sneaker-details'>
-                            {sneaker.brand} / {sneaker.silhouette} / {sneaker.name}
-                        </h1>
-                    </div>
-                    <div className='sneaker-show-right-container'>
-                        <Switch>
-                            <Route
-                                path="/sneakers/:sneakerId/listings/:listingId"
-                                render={(props) => <ListingIndexItem props={props} product={this.props.sneaker} listings={this.props.listings} currentUser={this.props.user} addItem={this.props.addItem} />}
-                            />
-                            <Route
-                                path="/sneakers/:sneakerId/listings/"
-                                render={() => <ListingIndex listings={Object.values(this.props.listings).sort(function (a, b) { return (a.size - b.size) })} type="sneakers"/>} 
-                            />
-                            <Route
-                                path="/sneakers/:sneakerId"
-                                render={() => <Sneaker props={this.props} displayPrice={this.displayPrice()}/>}
-                            />
-
-                        </Switch>
-                    </div>
-                </div>
-           
-                <div className='details-container'>
-                    <div className='description-container'>
-                        <h5 className='detail-header'>
-                            DETAILS 
-                            <div className='detail-icon'>
-                                <i className="fas fa-chevron-down"></i>
-                            </div>  
-                        </h5>
-                        <div className='detail-desciption'>
-                            {sneaker.description}
-                        </div>
-                    </div>
-                    <div className='details-pair-container'>
-                        <div>
-                            release date
-                        </div>
-                        <div>
-                            {sneaker.release_date}
-                        </div>
-                    </div>
-                    <div className='details-pair-container'>
-                        <div>
-                            Brand
-                        </div>
-                        <div>
-                            {sneaker.brand}
-                        </div>
-                    </div>
-                    <div className='details-pair-container'>
-                        <div>
-                            silhouette
-                        </div>
-                        <div>
-                            {sneaker.silhouette}
-                        </div>
-                    </div>
-                    <div className='details-pair-container'>
-                        <div>
-                            designer
-                        </div>
-                        <div>
-                            {sneaker.designer}
-                        </div>
-                    </div>
-                    <div className='details-pair-container'>
-                        <div>
-                            technology
-                        </div>
-                        <div>
-                            {sneaker.technology}
-                        </div>
-                    </div>
-                    <div className='details-pair-container'>
-                        <div>
-                            colorway
-                        </div>
-                        <div>
-                            {sneaker.colorway}
-                        </div>                    
-                    </div>
-                </div>
-         </div>
-        )
-    }
-
+        <div className="details-container">
+          <div className="description-container">
+            <h5 className="detail-header">
+              DETAILS
+              <div className="detail-icon">
+                <i className="fas fa-chevron-down"></i>
+              </div>
+            </h5>
+            <div className="detail-desciption">{sneaker.description}</div>
+          </div>
+          <div className="details-pair-container">
+            <div>release date</div>
+            <div>{sneaker.release_date}</div>
+          </div>
+          <div className="details-pair-container">
+            <div>Brand</div>
+            <div>{sneaker.brand}</div>
+          </div>
+          <div className="details-pair-container">
+            <div>silhouette</div>
+            <div>{sneaker.silhouette}</div>
+          </div>
+          <div className="details-pair-container">
+            <div>designer</div>
+            <div>{sneaker.designer}</div>
+          </div>
+          <div className="details-pair-container">
+            <div>technology</div>
+            <div>{sneaker.technology}</div>
+          </div>
+          <div className="details-pair-container">
+            <div>colorway</div>
+            <div>{sneaker.colorway}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default SneakerShow
+export default SneakerShow;
