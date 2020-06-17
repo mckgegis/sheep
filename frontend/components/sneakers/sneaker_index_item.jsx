6b6d78956c34853617e5ed25e44bee554db9ad72
mckgegis <mckgegis@yahoo.com>
2020-06-16 19:27:23 -0700

    stylecode

--- frontend/components/sneakers/sneaker_index_item.jsx
+++ frontend/components/sneakers/sneaker_index_item.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
+import React from "react";
 
 class SneakerIndexItem extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    
+  constructor(props) {
+    super(props);
+  }
 
-    render () {
-        return (
-            <div className='sneaker-item-container'>
-                <div className='sneaker-item'>
-                    <div className='sneaker-item-image-container'>
-                        <img src={this.props.sneaker.imageUrl} className='sneaker-item-image'/>
-                    </div>
-                    <div className='sneaker-item-name'>
-                        {this.props.sneaker.name}
-                    </div>
-                </div>
-
-            </div>
-        )
-    }
-
-    
+  render() {
+    return (
+      <div className="sneaker-item-container">
+        <div className="sneaker-item">
+          <div className="sneaker-item-image-container">
+            <img
+              src={this.props.sneaker.imageUrl}
+              className="sneaker-item-image"
+            />
+          </div>
+          <div className="sneaker-item-name">{this.props.sneaker.name}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default SneakerIndexItem
+export default SneakerIndexItem;
