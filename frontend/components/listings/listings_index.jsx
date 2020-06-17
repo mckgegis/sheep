6d8723e547b60ba06322303c23842578d5a47d47
mckgegis <mckgegis@yahoo.com>
2020-06-16 19:30:25 -0700

    stylecode

--- frontend/components/listings/listings_index.jsx
+++ frontend/components/listings/listings_index.jsx
@@ -1,36 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const ListingIndex = ({ listings, type } ) => (
-    
-    <div className='listings-index-container'>
-        <Link to={`/${type}/${listings[0].itemable_id}`} className='listings-index-x'>
-            <button>
-                <div>X</div>
-            </button>
-        </Link>
-        <div className="listing-header">
-            <h3>BUY NEW</h3>
-            <h1>US Men Sizes</h1>
-        </div>
-        {
-            listings.map(listing => {
-                return(
-                    <Link to={`/${type}/${listing.itemable_id}/listings/${listing.id}`}
-                        className='listing-index-item'
-                        key={listing.id}
-                        >
-                            <div className='listing-size'>
-                                <div>{listing.size}M</div>
-                            </div>
-                            <div className='listing-price'>
-                                <div>${listing.price}</div>
-                            </div>
-                    </Link>
-                )
-            })
-        }
+const ListingIndex = ({ listings, type }) => (
+  <div className="listings-index-container">
+    <Link
+      to={`/${type}/${listings[0].itemable_id}`}
+      className="listings-index-x"
+    >
+      <button>
+        <div>X</div>
+      </button>
+    </Link>
+    <div className="listing-header">
+      <h3>BUY NEW</h3>
+      <h1>US Men Sizes</h1>
     </div>
-)
+    {listings.map(listing => {
+      return (
+        <Link
+          to={`/${type}/${listing.itemable_id}/listings/${listing.id}`}
+          className="listing-index-item"
+          key={listing.id}
+        >
+          <div className="listing-size">
+            <div>{listing.size}M</div>
+          </div>
+          <div className="listing-price">
+            <div>${listing.price}</div>
+          </div>
+        </Link>
+      );
+    })}
+  </div>
+);
 
-export default ListingIndex
+export default ListingIndex;
