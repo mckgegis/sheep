5b2a22db90993de2db11fdb7e50b6e6e04af9762
mckgegis <mckgegis@yahoo.com>
2020-06-16 19:28:37 -0700

    stylecode

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,47 +1,48 @@
-import React from 'react'
-import SearchResults from './search_results'
-import { Link } from 'react-router-dom'
+import React from "react";
+import SearchResults from "./search_results";
+import { Link } from "react-router-dom";
 
 class Search extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
+  }
 
-       
-    }
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    this.props.clearSneaks();
+  }
 
-    componentDidMount(){
-        window.scrollTo(0, 0);
-        this.props.clearSneaks()
-    }
+  update() {
+    return e => {
+      this.props.fetchSneaks(e.currentTarget.value.toUpperCase());
+    };
+  }
 
-    update() {
-        return(e) => {
-            this.props.fetchSneaks(e.currentTarget.value.toUpperCase())
-        }
-    }
+  render() {
+    let { search } = this.props;
 
-    render() {
-
-        let { search } = this.props;
-
-        return(
-            <div className='search-container'>
-                <div className='search-bar'>
-                    <textarea className='search' placeholder="Type To Search" onChange={this.update()}></textarea>
-                </div>
-                <ul className="sneaker-index-items-container">
-                    {search.map((sneaker) => {
-                        return (
-                            <Link to={`/sneakers/${sneaker.id}`} key={sneaker.id}>
-                                <SearchResults sneaker={sneaker} key={sneaker.id} />
-                            </Link>
-                        )
-                    })}
-                </ul>
-            </div>
-        )
-    }
+    return (
+      <div className="search-container">
+        <div className="search-bar">
+          <textarea
+            className="search"
+            placeholder="Type To Search"
+            onChange={this.update()}
+          ></textarea>
+        </div>
+        <ul className="sneaker-index-items-container">
+          {search.map(sneaker => {
+            return (
+              <Link to={`/sneakers/${sneaker.id}`} key={sneaker.id}>
+                <SearchResults sneaker={sneaker} key={sneaker.id} />
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
-import { format } from 'url'
+import { format } from "url";
 
-export default Search
+export default Search;
--- frontend/components/sneakers/sneaker_index.jsx
+++ frontend/components/sneakers/sneaker_index.jsx
@@ -1,64 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SneakerIndexItem from './sneaker_index_item'
+import React from "react";
+import { Link } from "react-router-dom";
+import SneakerIndexItem from "./sneaker_index_item";
 
 class SneakerIndex extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.maxId = 0;
-        this.showMore = this.showMore.bind(this)
-        this.state = { show_more: true };
-    }
-    
-    componentDidMount() {
-        window.scrollTo(0, 0);
-        this.props.clearSneakers()
-        this.props.clearListings()
-        this.props.fetchSneakers(this.maxId)
-        this.maxId += 8
-    }
+    this.maxId = 0;
+    this.showMore = this.showMore.bind(this);
+    this.state = { show_more: true };
+  }
 
-    componentWillUnmount() {
-        this.props.clearSneakers()
-    }
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    this.props.clearSneakers();
+    this.props.clearListings();
+    this.props.fetchSneakers(this.maxId);
+    this.maxId += 8;
+  }
 
-    showMore() {
-        this.props.fetchSneakers(this.maxId)
-        .then(action => {
-            if (Object.values(action.sneakers).length < 8)
-                this.setState({ show_more: false });
-        });
-        this.maxId += 8
+  componentWillUnmount() {
+    this.props.clearSneakers();
+  }
 
-    }
-    render() {
-        const loadMore = this.state.show_more ? (
-            <button 
-                className="load-more-button" 
-                onClick={this.showMore}
-                >See More</button>
-        ) : (null)
-        return (
-            <div className='sneaker-container'>
-                <h1 className="sneaker-index-header">Shop All</h1>
-                <div className="sneaker-index-container">
-                    <ul className="sneaker-index-items-container">
-                        {this.props.sneakers.map((sneaker) => {
-                            return(
-                                <Link to={`/sneakers/${sneaker.id}`} key={sneaker.id}>
-                                    <SneakerIndexItem sneaker={sneaker} key={sneaker.id}/>
-                                </Link>
-                            )
-                        })}
-                    </ul>
-                </div>
-                <div className="load-more-container">
-                    {loadMore}
-                </div>
-            </div>
-        )
-    }
+  showMore() {
+    this.props.fetchSneakers(this.maxId).then(action => {
+      if (Object.values(action.sneakers).length < 8)
+        this.setState({ show_more: false });
+    });
+    this.maxId += 8;
+  }
+  render() {
+    const loadMore = this.state.show_more ? (
+      <button className="load-more-button" onClick={this.showMore}>
+        See More
+      </button>
+    ) : null;
+    return (
+      <div className="sneaker-container">
+        <h1 className="sneaker-index-header">Shop All</h1>
+        <div className="sneaker-index-container">
+          <ul className="sneaker-index-items-container">
+            {this.props.sneakers.map(sneaker => {
+              return (
+                <Link to={`/sneakers/${sneaker.id}`} key={sneaker.id}>
+                  <SneakerIndexItem sneaker={sneaker} key={sneaker.id} />
+                </Link>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="load-more-container">{loadMore}</div>
+      </div>
+    );
+  }
 }
 
-export default SneakerIndex
+export default SneakerIndex;
